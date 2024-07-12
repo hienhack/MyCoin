6b6b8d4872e1bfea77465ef70599049e998b5426
20120472 <hienthai.030102@gmail.com>
2024-07-12 22:29:59 +0700

    update wallet router

--- server/src/routes/wallet.route.js
+++ server/src/routes/wallet.route.js
@@ -1,53 +1,29 @@
 const express = require("express");
 const EC = require("elliptic");
-const walletModel = require("../schemas/wallet.schema");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const { JWT_SECRET } = require("../config");
 
 const router = express.Router();
 const ec = new EC.ec("secp256k1");
 
 router.post('/create', (req, res) => {
-    const { password } = req.body;
-    if (!password) {
-        res.status(400).json({ error: "Password is required" });
-    }
-
     const key = ec.genKeyPair();
     const publicKey = key.getPublic("hex");
     const privateKey = key.getPrivate("hex");
-
-    const privateKeyPassword = bcrypt.hashSync(password, 10);
-
-    // Save the wallet to the database
-    const wallet = new walletModel({ privateKeyPassword });
-    wallet.save();
 
     res.status(200).json({ publicKey, privateKey });
 });
 
 router.post('/access', async (req, res) => {
-    const { privateKey, password } = req.body;
-    if (!privateKey || !password) {
-        res.status(400).json({ error: "Private key is required" });
+    const { privateKey, publicKey } = req.body;
+    if (!privateKey || !publicKey) {
+        res.status(400).json({ error: "Public and private keys are required" });
     }
 
-    const privateKeyPassword = bcrypt.hashSync(password, 10);
-    try {
-        const wallet = await walletModel.findOne({ privateKeyPassword });
-    } catch (error) {
-        res.status(400).json({ error: "Private key or password incorrect!" });
+    const key = ec.keyFromPublic(publicKey, "hex");
+    if (!key.verify(publicKey, privateKey)) {
+        res.status(400).json({ error: "Invalid public key" });
     }
 
-    const key = ec.keyFromPrivate(privateKey);
-    const publicKey = key.getPublic("hex");
-
-    const payload = { privateKey };
-
-    const token = jwt.sign({ privateKey, }, JWT_SECRET);
-
-    res.status(200).json({ publicKey });
+    res.status(200).json({ privateKey, publicKey });
 });
 
 module.exports = router;
