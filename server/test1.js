896eeb27f8fc6157e5e40e6f2db19735fceccc11
20120472 <hienthai.030102@gmail.com>
2024-07-21 10:08:43 +0700

    remove test, done

--- server/test1.js
+++
@@ -1,9 +0,0 @@
-const Blockchain = require("./blockchain/Blockchain");
-const Node = require("./blockchain/Node");
-
-const { MyCoin } = require("./blockchain/MyCoin");
-
-const MyCoinNode = new Node(MyCoin, ["ws://localhost:3000"], "ws://localhost:3001", 3001);
-
-MyCoinNode.start();
-MyCoinNode.startMining("hienthai");
